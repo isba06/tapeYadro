51491e84ddb14a080fe09b419b9107609b4f8866
isba06 <www.ismail.com@mail.ru>
2023-03-01 01:09:50 +0300

    same

--- tape.h
+++ tape.h
@@ -1,3 +1,27 @@
 //
 // Created by Исмаил Байрамов on 26.02.2023.
 //
+
+#include <fstream>
+struct ITape {
+    virtual void forward() = 0;
+    virtual void back() = 0;
+    virtual void read() = 0;
+    virtual void write() = 0;
+    virtual ~ITape() = default;
+};
+
+struct Tape : public ITape {
+    void forward() override {
+
+    }
+
+    Tape(std::string filename) : file(filename)
+    {
+
+    }
+
+private:
+    std::fstream file;
+    std::size_t pos;
+};
--- test.cpp
+++ test.cpp
@@ -1,3 +1,7 @@
 //
 // Created by Исмаил Байрамов on 26.02.2023.
 //
+
+#include "gtest/gtest.h"
+#include "main.cpp"
+
